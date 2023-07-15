bced24ccf835b77778a289e2b9630aabecd7b3d4
MohamadMehdiK <MohamadMehdiK@SystemGroup.Net>
2023-07-16 00:01:37 +0330

    Add deep clone.

--- src/test/java/com/mmkarami/patterns/creational/prototype/StudentTest.java
+++ src/test/java/com/mmkarami/patterns/creational/prototype/StudentTest.java
@@ -3,9 +3,6 @@ package com.mmkarami.patterns.creational.prototype;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-
 import org.junit.jupiter.api.Test;
 
 public class StudentTest {
